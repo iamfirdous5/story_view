add075de94ed1bfe5c51f6a689191ad0badd94d8
iamfirdous5 <firdous@calibraint.com>
2022-07-18 14:27:11 +0530

    Cache loader removed for url images

--- lib/widgets/story_image.dart
+++ lib/widgets/story_image.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'dart:ui' as ui;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 import '../utils.dart';
@@ -12,78 +10,30 @@ import '../controller/story_controller.dart';
 /// Utitlity to load image (gif, png, jpg, etc) media just once. Resource is
 /// cached to disk with default configurations of [DefaultCacheManager].
 class ImageLoader {
-  ui.Codec? frames;
-
   String url;
-
-  Map<String, dynamic>? requestHeaders;
-
   LoadState state = LoadState.loading; // by default
-
   Image? networkImage;
-
   BoxFit? fit;
-
-  ImageLoader(this.url, {this.requestHeaders, this.fit});
+  ImageLoader(this.url, {this.fit});
 
   /// Load image from disk cache first, if not found then load from network.
   /// `onComplete` is called when [imageBytes] become available.
   void loadImage(VoidCallback onComplete) {
-    if (kIsWeb) {
-      networkImage = Image.network(url, fit: fit);
-      final onResult = (LoadState state) {
-        if (this.state != LoadState.loading) {
-          onComplete();
-          return;
-        }
-        this.state = state;
+    networkImage = Image.network(url, fit: fit);
+    final onResult = (LoadState state) {
+      if (this.state != LoadState.loading) {
         onComplete();
-      };
-      networkImage?.image.resolve(ImageConfiguration()).addListener(
-            ImageStreamListener(
-              (_, __) => onResult(LoadState.success),
-              onError: (_, __) => onResult(LoadState.failure),
-            ),
-          );
-
-      return;
-    }
-
-    if (this.frames != null) {
-      this.state = LoadState.success;
+        return;
+      }
+      this.state = state;
       onComplete();
-    }
-
-    final fileStream = DefaultCacheManager()
-        .getFileStream(this.url, headers: this.requestHeaders as Map<String, String>?);
-
-    fileStream.listen(
-      (fileResponse) {
-        if (!(fileResponse is FileInfo)) return;
-        // the reason for this is that, when the cache manager fetches
-        // the image again from network, the provided `onComplete` should
-        // not be called again
-        if (this.frames != null) {
-          return;
-        }
-
-        final imageBytes = fileResponse.file.readAsBytesSync();
-
-        this.state = LoadState.success;
-
-        PaintingBinding.instance!.instantiateImageCodec(imageBytes).then((codec) {
-          this.frames = codec;
-          onComplete();
-        }, onError: (error) {
-          this.state = LoadState.failure;
-          onComplete();
-        });
-      },
-      onError: (error) {
-        this.state = LoadState.failure;
-        onComplete();
-      },
-    );
+    };
+    networkImage?.image.resolve(ImageConfiguration()).addListener(
+          ImageStreamListener(
+            (_, __) => onResult(LoadState.success),
+            onError: (_, __) => onResult(LoadState.failure),
+          ),
+        );
   }
 }
 
@@ -116,7 +66,7 @@ class StoryImage extends StatefulWidget {
     Key? key,
   }) {
     return StoryImage(
-      ImageLoader(url, requestHeaders: requestHeaders, fit: fit),
+      ImageLoader(url, fit: fit),
       url,
       controller: controller,
       fit: fit,
@@ -141,26 +91,19 @@ class StoryImageState extends State<StoryImage> {
     if (widget.controller != null) {
       this._streamSubscription =
           widget.controller!.playbackNotifier.listen((playbackState) {
-        // for the case of gifs we need to pause/play
-        if (widget.imageLoader.frames == null) {
-          return;
-        }
-
         if (playbackState == PlaybackState.pause) {
           this._timer?.cancel();
         } else {
-          forward();
+          setState(() {});
         }
       });
     }
-
     widget.controller?.pause();
-
     widget.imageLoader.loadImage(() async {
       if (mounted) {
         if (widget.imageLoader.state == LoadState.success) {
           widget.controller?.play();
-          forward();
+          setState(() {});
         } else {
           // refresh to show error
           setState(() {});
@@ -184,38 +127,13 @@ class StoryImageState extends State<StoryImage> {
     }
   }
 
-  void forward() async {
-    if (kIsWeb) {
-      setState(() {});
-      return;
-    }
-    this._timer?.cancel();
-
-    if (widget.controller != null &&
-        widget.controller!.playbackNotifier.stream.value == PlaybackState.pause) {
-      return;
-    }
-
-    final nextFrame = await widget.imageLoader.frames!.getNextFrame();
-
-    this.currentFrame = nextFrame.image;
-
-    if (nextFrame.duration > Duration(milliseconds: 0)) {
-      this._timer = Timer(nextFrame.duration, forward);
-    }
-
-    setState(() {});
-  }
-
   Widget getContentView() {
     final fail = Center(
       child: Text('Image failed to load.', style: TextStyle(color: Colors.white)),
     );
     switch (widget.imageLoader.state) {
       case LoadState.success:
-        return kIsWeb
-            ? widget.imageLoader.networkImage ?? fail
-            : RawImage(image: currentFrame, fit: widget.fit);
+        return widget.imageLoader.networkImage ?? fail;
       case LoadState.failure:
         return fail;
       default:
